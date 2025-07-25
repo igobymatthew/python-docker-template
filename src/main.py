bb2729380d16ba4393a58cc76e27e6591123e2ba
Matthew Ballard <68800757+igobymatthew@users.noreply.github.com>
2025-07-25 13:54:57 -0400

    Add baseline training pipeline

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Template Python app is running in Docker.")
+# %%
+print("Jigsaw Agile modeling utilities.")
+
+# %%
+if __name__ == "__main__":
+    print("Use train.py to run model training.")
+
